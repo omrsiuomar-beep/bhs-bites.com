9b2e37f8f623b527083b468806ce200b2ae18170
Omar yasser <omrsiuomar@gmail.com>
2026-09-22 03:03:23 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'bhs-bites-v1';
+const CORE = ['./', './index.html', './install.html', './manifest.webmanifest', './app-icon-192.png', './app-icon-512.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(hit => hit || caches.match('./index.html')))
+  );
+});
